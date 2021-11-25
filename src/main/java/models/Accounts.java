bf8ed15bf5aa621d1c937757292cf007c12d38a0
Alfred Garcia <garcia.alfredm@gmail.com>
2021-11-24 23:31:26 -0500

    created models

---
+++ src/main/java/models/Accounts.java
@@ -0,0 +1,61 @@
+package models;
+
+import java.util.Date;
+
+public class Accounts {
+    private Integer id;
+    private Integer clientIdFk;
+    private Double balance;
+    private Date accountDateCreation;
+
+    public Accounts(){
+        this.balance = 0.00;
+    }
+
+    public Accounts(Integer clientIdFk, Double balance) {
+        this.clientIdFk = clientIdFk;
+        this.balance = balance;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getClientIdFk() {
+        return clientIdFk;
+    }
+
+    public void setClientIdFk(Integer clientIdFk) {
+        this.clientIdFk = clientIdFk;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
+
+    public Date getAccountDateCreation() {
+        return accountDateCreation;
+    }
+
+    public void setAccountDateCreation(Date accountDateCreation) {
+        this.accountDateCreation = accountDateCreation;
+    }
+
+    @Override
+    public String toString() {
+        return "Accounts{" +
+                "id=" + id +
+                ", clientIdFk=" + clientIdFk +
+                ", balance=" + balance +
+                ", accountDateCreation=" + accountDateCreation +
+                '}';
+    }
+}
---
+++ src/main/java/models/Clients.java
@@ -0,0 +1,48 @@
+package models;
+
+import java.util.Date;
+
+public class Clients {
+    private Integer id;
+    private String name;
+    private Date clientDateCreation;
+
+    public Clients(){}
+
+    public Clients(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getClientDateCreation() {
+        return clientDateCreation;
+    }
+
+    public void setClientDateCreation(Date clientDateCreation) {
+        this.clientDateCreation = clientDateCreation;
+    }
+
+    @Override
+    public String toString() {
+        return "Clients{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", clientDateCreation=" + clientDateCreation +
+                '}';
+    }
+}
